4874a1d64a8c430ce6c3ceb12641c3d410e531ac
Maaltas <107067687+Maaltas@users.noreply.github.com>
2023-02-01 19:40:27 +0100

    Update TaulellSessio2.java

--- TaulellSessio2.java
+++ TaulellSessio2.java
@@ -4,50 +4,69 @@ public class Taulell {
     private Paraula [][] taulell;
     private int parelles;
     private static final String[] mots = {"pinya     ", "Kiwi     ", "cirera     ", "maduixa     ", "melo     ", "llimona     ", "mandarina     ", "pepino     ", "platan     ", "chirimoia     ", "poma     ", "granada     ", "raim     ", "pera     ", "taronja     ", "papaya     ", "sindria     ", "fambruesa     "};
+    private static final String[] usedWords = new String [36];
 
     public Taulell (int fil, int col){
         Paraula taulell[][]= new Paraula [fil][col];
     }
-    public void emplena(){
+    private void emplena(){
         Random var = new Random();
         String s;
-        int fil= taulell.length;
         for (int x=0; x< taulell.length;x++){
             for (int y=0; y<taulell[x].length;y++) {
-                s=mots[var.nextInt((fil)*(taulell[x].length)/2)];
-                char[] c = s.toCharArray();
-
+                s=mots[var.nextInt((taulell.length)*(taulell[x].length)/2)];;
+                taulell[x][y]=new Paraula(s);
+                while (quants(s)){
+                    s=mots[var.nextInt((taulell.length)*(taulell[x].length)/2)];
+                    taulell[x][y]=new Paraula(s);
+                }
             }
         }
-
     }
-    /*
-    for (int x=0;x<taulell.length;x++){
-            for (int y=0;y<taulell[x].length;y++){
-                s=paraules[var.nextInt((fil*col)/2)];
-                t[x][y]=s;
-                while (quants(s,t)>0){
-                    s=paraules[var.nextInt((fil*col)/2)];
-                }
-                t[x][y]=s;
-            }
-        }
-     */
-    /*
-    int quantes=0;
-        boolean trobat=false;
-        for (int x=0; x<taulell.length && !trobat;x++){
-            for(int y=0; y<taulell[x].length;y++){
-                if (parula.equals(taulell[x][y])){
-                    quantes++;
-                }
-                if (quantes == 2) {
-                    return quantes;
+    private boolean quants(String paraula){
+        int quants=0;
+        for (int x=0; x<taulell.length;x++) {
+            for (int y = 0; y < taulell[x].length; y++) {
+                if(usedWords[x].equals(paraula)){
+                    quants++;
+                }  
+                if (quants==2){
+                    return true;
                 }
             }
         }
-     */
+        return false;
+    }
+
+    public void mostrar(int fil1, int col1, int fil2, int col2){
+        for(int x=0;x< taulell.length;x++){
+            for(int y=0; y< taulell[x].length;y++){
+                if(taulell[x][y].getParella() && taulell[fil1][col1].getParella()){
+                    System.out.print(taulell[x][y]);
+                } else if (taulell[x][y].getParella() && taulell[fil2][col2].getParella()){
+                    System.out.print(taulell[x][y]);
+                } else {
+                    System.out.print("TAPAT       ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+    public boolean fanParella(int fil1, int col1, int fil2, int col2){
+        if(taulell[fil1][col1].equals(taulell[fil2][col2])){
+            taulell[fil1][col1].setParella();
+            taulell[fil2][col2].setParella();
+            return true;
+        }
+        return false;
+    }
+    public boolean aparellada(int fil, int col){
+        return taulell[fil][col].getParella();
+    }
+    public boolean fiJoc(){
+        if (parelles==((taulell.length*taulell[0].length)/2)){
+            return true;
+        }
+        return false;
+    }
 }
-
-
-
